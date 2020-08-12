0b369c0d3eb7ff60cf9155c66c7e53f82f3aa98c
Cody Frank <cody.frank30@gmail.com>
2020-08-12 11:16:49 -0400

    create bomb array and empty array

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let width = 10
     let squares = []
+    let bombAmount = 20
+
+
+
     //  create board
     function createBoard() {
+        // get shuffled game array with random bombs
+        const bombArray = Array(bombAmount).fill('bomb')
+        const emptyArray = Array(width*width - bombAmount).fill('valid')
+        console.log(bombArray)
+        console.log(emptyArray)
+
         for(let i = 0; i < width*width; i++){
             const square = document.createElement('div')
             square.setAttribute("id", i)
